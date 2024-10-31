949c071f5a6fc54d3adfe9795cafa5d460027a28
ibrahimdeniz24 <160463714+ibrahimdeniz24@users.noreply.github.com>
2024-10-31 13:21:01 +0300

    Güncel veriler eklendi, Bir kaç değişiklikler yapıldı.

--- MyResume.Infrastructure/Migrations/20241031093922_20241031_003.Designer.cs
+++ MyResume.Infrastructure/Migrations/20241031093922_20241031_003.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyResume.Infrastructure.AppContext;
 
@@ -11,9 +12,11 @@ using MyResume.Infrastructure.AppContext;
 namespace MyResume.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241031093922_20241031_003")]
+    partial class _20241031_003
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
